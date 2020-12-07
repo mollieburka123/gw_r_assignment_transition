0aef83fcb4d190c0d435b092b5010eb9445da6b4
Mollie Wormser <molliewormser@mollies-air.myfiosgateway.com>
2020-12-07 12:00:28 -0500

    commit 3

--- 03_compare_current_with_previous.R
+++ 03_compare_current_with_previous.R
@@ -24,6 +24,11 @@ source("02_scrape_nominees.R")
 
 #Your code here#
 
+staff_data_current <- readRDS("processed_data/staff_data_scraped.rds")
+staff_data_current
+
+staff_data_previous <- readRDS("archived_data/staff_data_archived_2020_11_24t14_00.rds")
+staff_data_previous
 
 #find new records of names added since previous
 newnames <- anti_join(transition_data_scraped, transition_data_archived_2020_11_24t09_52, by = "idstring")
@@ -31,38 +36,24 @@ newnames <- anti_join(transition_data_scraped, transition_data_archived_2020_11_
 #see what we have
 newnames
 
+#11 new 
+
+
 # Compare TOTALS by department #######
-agencycount_current <- transition_data_scraped %>% 
-  count(agency, name = "current_count")
 
-agencycount_current
+title_count_current <- staff_data_scraped %>%
+  count(title, name="current_count")
 
-agencycount_previous <- transition_data_archived_2020_11_24t09_52 %>% 
-  count(agency, name = "previous_count")
+title_count_previous <- staff_data_archived_2020_11_24t14_00 %>%
+  count(title, name="previouscount")
 
-agencycount_previous
 
 
 #join
-agencycount_compare <- left_join(agencycount_current, agencycount_previous, by = "agency")
-agencycount_compare
 
-#add change columns
-agencycount_compare <- agencycount_compare %>% 
-  mutate(
-    change = current_count - previous_count
-  )
+title_count_compare <- left_join(title_count_current, title_count_previous, by = "title")
 
-#we'll create a NEW NAMED OBJECT to use from here on out for the full dataset
-agencyteams <- transition_data_scraped
-
-#names of new agency review team members
-saveRDS(newnames, "processed_data/newnames.rds")
-#aggregate county of agency totals compared
-saveRDS(agencycount_compare, "processed_data/agencycount_compare.rds")
-#entire combined agency teams file
-saveRDS(agencyteams, "processed_data/agencyteams.rds")
-
+#done
 
 
 #### AGENCY TEAMS ##### --------------------------------------------------------
